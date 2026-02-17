55936145462f28143acef8b8c94c2830530cff42
artemchik12 <128714855+artemchik12@users.noreply.github.com>
2026-02-17 22:22:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    HOST: '192.168.0.15',
+    HOST: '0.0.0.0',
     AUTH_PORT: 5190,
     BOS_PORT: 5191,
-    // ВАЖНО: Укажите здесь ваш реальный IP, если подключаетесь не с localhost
-    BOS_ADDRESS: '192.168.0.15', 
+    BOS_ADDRESS: '127.0.0.1',
     DB_PATH: './icq.db',
-    DEBUG_PACKETS: true, // Включает лог пакетов
+    DEBUG_PACKETS: true,
+
+    REGISTRATION_ENABLED: true,
+    UIN_MIN: 100000,
+    UIN_MAX: 999999,
+
+    // ═══════════════════════════════════════
+    //  Admin Panel Config
+    // ═══════════════════════════════════════
+    ADMIN_USER: 'admin',
+    ADMIN_PASS: 'secret', // Change this!
 };
